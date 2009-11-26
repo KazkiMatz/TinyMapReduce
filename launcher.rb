686118652b3c3e681c3d9af821800af53530586d
Kazuki MATSUMOTO <deliciouskid@gmail.com>
2009-11-26 16:37:22 +0900

    Day 1

--- launcher.rb
+++ launcher.rb
@@ -3,8 +3,6 @@ require 'drb/drb'
 module TinyMapReduce
   class Launcher
     def self.start uri
-      $tt_conn = PureMemcached.new(TT_HOST[0], {:timeout => 30})
-
       DRb.start_service(uri, Worker.new)
       puts DRb.uri
       sleep
